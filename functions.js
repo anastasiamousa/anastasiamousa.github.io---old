4ca9c8a450db4ae844609ee62f736851230c4a6c
Anastasia Mousa <49337566+anastasiamousa@users.noreply.github.com>
2021-01-06 20:20:20 +0200

    uploading Ana's Place files

---
+++ functions.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    const menu = document.querySelector('.toggle')
+    const navigation = document.querySelector('.navigation')
+    menu.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
+
+window.onload = function() {
+    var loading = document.getElementById('loading')
+    loading.style.marginLeft = "20px";
+}
