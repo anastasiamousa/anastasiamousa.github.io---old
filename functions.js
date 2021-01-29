a431acf70e0c810a63e20ff4cade75d6b1de8545
anastasiamousa <anastasiamousa34@gmail.com>
2021-01-29 22:43:39 +0200

    edits

--- functions.js
+++ functions.js
@@ -1,11 +1,11 @@
 function toggleMenu() {
-    const menu = document.querySelector('.toggle')
-    const navigation = document.querySelector('.navigation')
-    menu.classList.toggle('active')
-    navigation.classList.toggle('active')
+    var menu = document.querySelector('.toggle');
+    var navigation = document.querySelector('.navigation');
+    menu.classList.toggle('active');
+    navigation.classList.toggle('active');
 }
 
 window.onload = function() {
-    var loading = document.getElementById('loading')
-    loading.style.marginLeft = "8px"
+    var loading = document.getElementById('loading');
+    loading.style.marginLeft = "8px";
 }
